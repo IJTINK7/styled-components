8fd27acae3035c00eadd0a5fea5eea159a8835c5
IJTINK7 <ijtink7@mail.ru>
2024-02-26 21:51:17 +0300

    Creating div and buttons via styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Box>
+				<StyledButton>Hello</StyledButton>
+				<SuperButton>SuperButton</SuperButton>
+			</Box>
+		</div>
+	);
 }
 
 export default App;
+
+const StyledButton = styled.button`
+  border: none;
+  background-color: #3459ff;
+  padding: 10px 20px;
+  color: snow;
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+const SuperButton = styled(StyledButton)`
+  border-radius: 5px;
+  background-color: #ff2525;
+`
+
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
