a3b16037a21e307655c2c35c1f7dbb2ad636b3dc
IJTINK7 <ijtink7@mail.ru>
2024-02-26 22:06:34 +0300

    Creating link hover and enclosure via styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
 	return (
 		<div className="App">
 			<Box>
+				<StyledButton as='a' href={"#"}>Link</StyledButton>
+				<StyledButton as={Link} href={"#"}>LinkComponent</StyledButton>
 				<StyledButton>Hello</StyledButton>
 				<SuperButton>SuperButton</SuperButton>
 			</Box>
@@ -22,16 +24,40 @@ const StyledButton = styled.button`
   color: snow;
   font-size: 2rem;
   font-weight: bold;
+
+  &:hover {
+    background-color: #d5c31e;
+  }
+
+  &:last-child {
+    background-color: #44a928;
+  }
 `
+const Link = styled.a`
+  color: #0cb40c;
+  font-size: 2rem;
+  font-weight: bold;
+  background-color: transparent;
+  padding: 0;
+`
+
+
 
 const SuperButton = styled(StyledButton)`
   border-radius: 5px;
   background-color: #ff2525;
 `
-
 const Box = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 20px;
+  
+  button{
+	cursor: pointer;
+  } 
+  ${Link}{
+	cursor: zoom-in;
+  }
 `
